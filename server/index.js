b1b0e2a83e7d9be9b5e119d2c479e5a4f94f56be
gerantonyk <ger.antonyk@gmail.com>
2021-10-09 18:52:22 -0300

    initial commit

--- server/index.js
+++ server/index.js
@@ -3,17 +3,33 @@ const app = express();
 const cors = require('cors');
 const port = 3042;
 
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+
 // localhost can have cross origin errors
 // depending on the browser you use!
 app.use(cors());
 app.use(express.json());
 
-const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
+const key = ec.genKeyPair();
+console.log("key",key);
+const privKey  = key.getPrivate()//.toString(16)//toHex;
+console.log("PrivateKey",privKey);
+const publicKey  = key.getPublic().encode('hex');
+console.log("PublicKey",publicKey);
+const publicKey2 = ec.keyFromPrivate(privKey).getPublic().encode('hex')
+console.log("public",publicKey2)
+initialAccounts = []
+for (let i = 0; i < 3; i++) {
+  initialAccounts.push(ec.genKeyPair().getPublic().encode('hex'));
+  
 }
-
+const balances = {
+  [initialAccounts[0]]: 100,
+  [initialAccounts[1]]: 50,
+  [initialAccounts[2]]: 75,
+}
+console.log(balances)
 app.get('/balance/:address', (req, res) => {
   const {address} = req.params;
   const balance = balances[address] || 0;
